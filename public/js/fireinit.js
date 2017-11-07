27721bb86e235357496867843ce7a1e2a8f12848
ITKOO <j4550118@e-mirm.hs.kr>
2017-11-07 21:21:21 +0900

    #지원서 폼 완성

---
+++ public/js/fireinit.js
@@ -0,0 +1,13 @@
+
+ var config = 
+ {
+    apiKey: "AIzaSyCaHUFiZDi5XOZk_T9Z5CB_wp81vTRwir8",
+    authDomain: "pratice-d74ad.firebaseapp.com",
+    databaseURL: "https://pratice-d74ad.firebaseio.com",
+    projectId: "pratice-d74ad",
+    storageBucket: "pratice-d74ad.appspot.com",
+    messagingSenderId: "870267536799"
+  };
+  
+  firebase.initializeApp(config);
+
